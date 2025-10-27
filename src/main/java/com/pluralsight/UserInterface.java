d54de4c6e0268c50c1edc9e889f1ae15103250f7
Student <wardahjavid62@gmail.com>
2025-10-27 12:00:04 -0400

    This is a method which sets up Scanner for user input in public class UserInterface.

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,10 +1,15 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class UserInterface {
     private Dealership dealership;
     public void display() {
         DealershipFileManager dfm = new DealershipFileManager();
         this.dealership = dfm.getDealership();
+
+        Scanner potatoscanner = new Scanner(System.in);
+        String choice = "";
     }
 
 }
