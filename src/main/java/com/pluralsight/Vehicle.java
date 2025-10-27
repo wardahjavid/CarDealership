d7dd5e9c4a0497bd5cc05c092ecbffebc81c24e0
Student <wardahjavid62@gmail.com>
2025-10-27 01:53:01 -0400

    Adding Attributes for public class Vehicle

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String type;
+    private String color;
+    private int odometer;
+    private double price;
+
+
+}
+
