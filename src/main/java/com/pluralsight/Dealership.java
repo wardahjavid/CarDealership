1f3717122f5f7c2814c3c1408e25da82785c6a0f
Student <wardahjavid62@gmail.com>
2025-10-27 02:44:41 -0400

    Adding attributes for public class Dealership

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> inventory;
+}
