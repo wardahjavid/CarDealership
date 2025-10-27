dc91dc810f42b88ef8c761a91550d2c162854663
Student <wardahjavid62@gmail.com>
2025-10-27 13:22:12 -0400

    Adding a while loop to loop through menu options till user exits program in public class UserInterface.

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -9,7 +9,30 @@ public class UserInterface {
         this.dealership = dfm.getDealership();
 
         Scanner potatoscanner = new Scanner(System.in);
-        String choice = "";
+        String option = "";
+
+        while (!option.equals("99")) {
+            displayMenu();
+            option = potatoscanner.nextLine();
+
+            switch (option) {
+                case "1": processGetByPrice(); break;
+                case "2": processGetByMakeModel(); break;
+                case "3": processGetByYear(); break;
+                case "4": processGetByColor(); break;
+                case "5": processGetByMileage(); break;
+                case "6": processGetByType(); break;
+                case "7": displayVehicles(dealership.getAllVehicles()); break;
+                case "8": processAddVehicle(); break;
+                case "9": processRemoveVehicle(); break;
+                case "99":
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("This is an invalid choice.");
+                    break;
+            }
+        }
     }
 
 }
